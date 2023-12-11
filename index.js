070a05dafec539de2db02885e24eb2c41f012dca
Kasiet Tokbaeva <kasiet.tokbaeva.kk@gmail.com>
2023-12-11 13:56:41 +0600

    31github

---
+++ index.js
@@ -0,0 +1,17 @@
+/*et lang = prompt("Введите ваш язык (ru) (ky)");
+if (lang==="ru") {document.write("Здравствуйте!");}
+else if (lang==="ky") {document.write("Саламатсызбы");}
+else {document.write("Я не знаю такого языка");}*/
+
+
+/*let title = prompt("Введите название месяца");
+if (title==="декабрь" || title==="январь" || title==="февраль" ) {document.write("Это зима!");}
+else if (title==="март" || title==="апрель" || title==="май" ) {document.write("Это весна!");}
+else if (title==="июнь" || title==="июль" || title==="август" ) {document.write("Это лето!");}
+else if (title==="сентябрь" || title==="октябрь" || title==="ноябрь" ) {document.write("Это осень!");}*/
+
+
+
+
+
+
